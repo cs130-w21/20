bd0925f233588e75236bda7ab20c2bdb7ae8bed6
elrodrigues <44657527+elrodrigues@users.noreply.github.com>
2021-02-01 17:43:29 -0600

    Input, Delete multiple stock symbols, No DB

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,7 +11,7 @@ Sandbox API Key: sandbox_c0bfrg748v6to0roveg0
 Regular API Key: c0bfrg748v6to0roveg0
 
 There are limits! See documentation:
-https://finnhub.io/docs/api/upgrade-downgrade
+https://finnhub.io/docs/api
 """
 
 def create_app():
@@ -35,26 +35,40 @@ def create_app():
 				values['q'] = int(os.stat(file_path).st_mtime)
 		return url_for(endpoint, **values)
 
-
 	# Home page
 	@app.route('/', methods=['GET', 'POST'])
 	def index():
 		if request.method == 'POST':
-			# TODO: input validation
+			# DONE: input validation
 			stock_symbol = request.form['stock'].upper()
 			volume = request.form['volume']
 			symbol_quote = finnhub_client.quote(stock_symbol)
 			error = None
-			if symbol_quote['c'] == 0:
+			if not volume.isdigit():
+				error = "Number of Shares must be a positive integer"
+			elif int(volume) < 1:
+				error = "Number of Shares must be a positive integer"
+			elif symbol_quote['c'] == 0:
 				error = "Invalid stock symbol: {}".format(stock_symbol)
-			
+
 			# TODO: Send form results to DB, fetch all added stocks and display them
+			# Code below uses Flask session to store data which can be moved to
+			# DB later.
 			if error is None:
-				dummy_stock_dict = {stock_symbol: volume} 
-				return render_template('index.html', stock_dict=dummy_stock_dict)
+				if 'stock_dict' in session:
+					(session['stock_dict'])[stock_symbol] = volume
+					session.modified = True
+				else:
+					session['stock_dict'] = {stock_symbol: volume}
+				return render_template('index.html', stock_dict=session['stock_dict'])
+			
 			flash(error)
+			if 'stock_dict' in session and session['stock_dict']:
+				return render_template('index.html', stock_dict=session['stock_dict'])
 			return render_template('index.html', stock_dict=None)
 		else:
+			if 'stock_dict' in session and session['stock_dict']:
+				return render_template('index.html', stock_dict=session['stock_dict'])
 			return render_template('index.html', stock_dict=None)
 
 	# Results page (generates session id or code)
@@ -70,7 +84,14 @@ def create_app():
 			# Person sends link to partner
 			print(session)
 			return render_template('results.html', code=code)
-			
+	
+	# Remove stock symbol from table
+	@app.route('/remove/<key>')
+	def remove(key):
+		if 'stock_dict' in session:
+			session['stock_dict'].pop(key, None)
+			session.modified = True
+		return redirect(url_for('index'))
 
 	@app.route('/waiting/<code>')
 	def wait_for_partner(code):
