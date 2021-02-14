9c2eb6cf01a6f99e30144372ca6998ea5378e8fe
elrodrigues <44657527+elrodrigues@users.noreply.github.com>
2021-02-14 17:22:10 -0600

    outlined profile generate

--- flaskr/algorithm.py
+++ flaskr/algorithm.py
@@ -1,5 +1,38 @@
-def generate_profile(portfolio):
-    pass
+def generate_profile(portfolio, finnhub_client):
+    """
+    Generates personality profile from portfolio
 
-def compare_profiles(p1, p2):
+    Parameters
+    ----------
+    portfolio : (str, int) dict
+        Dictionary of stock symbol and number held
+        pairs.
+    finnhub_client : finnhub.Client
+        Finnhub API client for financial data
+
+    Returns
+    -------
+    (str, int) dict
+        Dictionary of personality factors and
+        values.
+    
+    Notes
+    -------
+    Dependent Var MB, SIZE regression values
+    EXPEXT 0.022 ; 1277
+    IMPDIS 0.057 ; -749
+    SENTIM -0.054 ; 63
+    ATTDEP -0.017 ; -1210
+    Method of converting: OLS snip?
+    """
+    
+    personality = {}
+    total = 0
+    #TODO: implement OLS snipping
+    for symbol, num in portfolio:
+        total = total + num
+
+    return personality
+
+def compare_profiles(p1, p2, finnhub_client):
     pass
