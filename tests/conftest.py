1619daf85cf8f11f20d9dcf4bf2772296c6590bd
elrodrigues <44657527+elrodrigues@users.noreply.github.com>
2021-02-25 17:43:33 -0600

    modified conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,28 @@
 import os, pytest
+import tempfile
 from flaskr import create_app
+from flaskr.db import init_db
 
 @pytest.fixture
 def app():
-	app = create_app()
+	db_fd, db_path = tempfile.mkstemp()
+
+	app = create_app({
+		'TESTING': True,
+		'DATABASE': db_path,
+	})
+	with app.app_context():
+		init_db()
 	yield app
+
+	os.close(db_fd)
+	os.unlink(db_path)
 
 
 @pytest.fixture
 def client(app):
 	return app.test_client()
+
+@pytest.fixture
+def runner(app):
+	return app.test_cli_runner()
