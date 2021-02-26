6845041c4c9d16a0380d531093114d337ecc5080
elrodrigues <44657527+elrodrigues@users.noreply.github.com>
2021-02-25 18:27:09 -0600

    Forgot to import get_db
    I should run pytest more often lol

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import os, pytest
 import tempfile
 from flaskr import create_app
-from flaskr.db import init_db
+from flaskr.db import get_db, init_db
 
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
 	_data_sql = f.read().decode('utf8')
