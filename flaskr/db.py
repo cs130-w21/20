22e710b3033c63310c298079afdf1915dbc08060
khzheng24 <kennyzheng24998@gmail.com>
2021-02-28 15:48:00 -0800

    Added API documentation for db.py

--- flaskr/db.py
+++ flaskr/db.py
@@ -6,6 +6,21 @@ from flask.cli import with_appcontext
 # API for reading/writing to sqlite db
 
 def get_profile(user_id):
+    """
+    Queries SQLite database for profile by user ID
+
+    Arguments
+    ---------
+    user_id : string
+        User ID of profile to query
+
+    Returns
+    -------
+    sqlite3.Row
+        A Row object containing the query result, or
+        None if a row with uid = user_id does not exist
+    """
+
     profile = get_db().execute(
             'SELECT * FROM profiles WHERE uid = ?', (user_id,)
         ).fetchone()
@@ -13,6 +28,24 @@ def get_profile(user_id):
     return profile
 
 def create_profile(user_id, profile={}):
+    """
+    Creates a new row in the profiles table of the SQLite database
+
+    Arguments
+    ---------
+    user_id : string
+        User ID of profile
+    profile : (strint, int) dict
+        Profile containing personality factors and values
+        created by algorithm.generate_profile()
+
+    Notes
+    -----
+    Profile is serialized into a JSON string using json.dumps()
+    before storing into the database (SQLite does not support
+    storing of complex data structures)
+    """
+
     db = get_db()
     db.execute(
         'INSERT INTO profiles (uid, profile)'
@@ -20,11 +53,20 @@ def create_profile(user_id, profile={}):
         (user_id, json.dumps(profile))
     )
     db.commit()
-    return True
 
 # Tutorial code from https://flask.palletsprojects.com/en/1.1.x/tutorial/database/
 
 def get_db():
+    """
+    Initializes connection to SQLite database if one hasn't already
+    been created, otherwise returns the singleton Connection object
+
+    Returns
+    -------
+    sqlite3.Connection
+        A Connection object to the SQLite database
+    """
+
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
@@ -35,6 +77,8 @@ def get_db():
     return g.db
 
 def close_db(e=None):
+    """Closes connection to SQLite database"""
+
     db = g.pop('db', None)
 
     if db is not None:
