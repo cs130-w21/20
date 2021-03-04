1cc34f66f83b18686b61a6e6056da0f51920e76b
khzheng24 <kennyzheng24998@gmail.com>
2021-03-03 21:15:21 -0800

    Fixed broken test cases in test_compare.py

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -5,14 +5,14 @@ TEST_ID2 = 'A1B2C3D4E5'
 ENCODING = 'utf-8'
 
 def test_get_compare(client):
-    get_response = client.get('compare', follow_redirects=True)
+    get_response = client.get('compare/<placeholder>', follow_redirects=True)
     assert b"Compare Portfolios" in get_response.data
 
 # PLACEHOLDER TEST
 # TODO: Implement this in Sprint 3
 def test_post_compare(client):
     # Enter same ID
-    post_response = client.post('compare', data=dict(
+    post_response = client.post('compare/<placeholder>', data=dict(
             person1=TEST_ID1,
             person2=TEST_ID1
     ), follow_redirects=True)
@@ -20,7 +20,7 @@ def test_post_compare(client):
     assert b"IDs cannot be the same!" in post_response.data
 
     # test if IDs are invalid/have no DB entries
-    post_response = client.post('compare', data=dict(
+    post_response = client.post('compare/<placholder>', data=dict(
             person1=TEST_ID1,
             person2='ZZZZZZZZZZ'
     ), follow_redirects=True)
@@ -30,7 +30,7 @@ def test_post_compare(client):
     
     # no portfolio in session, so should redirect to index
     # TODO: implement this when Compare is implemented
-    post_response = client.post('compare', data=dict(
+    post_response = client.post('compare/<placeholder>', data=dict(
             person1=TEST_ID1,
             person2=TEST_ID2
     ), follow_redirects=True)
